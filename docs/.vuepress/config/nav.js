672c6b13dd7614d0bb83d96de632561f11b8a726
nick <528827908@qq.com>
2023-08-08 14:29:54 +0800

    add WINDBG 文章

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -26,8 +26,7 @@ module.exports = [
     
     items: [
       {text: '技术文档', link: '/pages/9a7ee40fc232253e/'},
-  
-     // {text: 'windbg', link: '/pages/4c778760be26d8b0/'},
+      {text: 'WINDBG', link: '/pages/41f87d890d0a02af/'},
           /*
       {text: 'Nodejs', link: '/pages/117708e0af7f0bd9/'},
       {text: '博客搭建', link: '/pages/41f87d890d0a02af/'},
