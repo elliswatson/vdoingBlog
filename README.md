a5d0227f68073b4538ac3b1e34f8d533844a6d70
nick <528827908@qq.com>
2023-08-14 11:48:47 +0800

    1.修改 README.md 2.更新deploy.sh 每次发布会自动删除之前的gh-pages分支

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-# elliswatson.github.io/vdoingBlog/
-=======
 # elliswatson.github.io/vdoingBlog
->>>>>>> 981901acd6f1744e822e67aecbe0f11742d409d4
 一个记录自己的技能树的博客
 [点此在线浏览](https://elliswatson.github.io/vdoingBlog/)
 
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env sh
 
+git pull -r
+
 # 确保脚本抛出遇到的错误
+if [  $(git branch -r | grep origin/gh-pages)=="origin/gh-pages" ];
+then 
+      git push origin --delete gh-pages
+else
+    echo "no branch gh-pages"   
+fi
+
+
 set -e
 
 # 生成静态文件
@@ -12,8 +22,8 @@ cd docs/.vuepress/dist
 # deploy to github
 #echo 'b.miluluyo.github.io' > CNAME
 if [ -z "$VDOING" ]; then
-  msg='deploy'
-  githubUrl=git@github.com:elliswatson/vdoingBlog.git
+   msg='deploy'
+   githubUrl=git@github.com:elliswatson/vdoingBlog.git
 else
   msg='来自 github actions的自动部署'
   githubUrl=https://elliswatson:${VDOING}github.com:elliswatson/vdoingBlog.git
@@ -24,19 +34,8 @@ git init
 git add -A
 git commit -m "${msg}"
 git push -f $githubUrl master:gh-pages # 推送到github
-#git push -f git@github.com:miluluyo/vdoingBlog.git master:gh-pages
 
-
-# deploy to coding
-#echo 'www.miluluu.com\miluluu.com' > CNAME  # 自定义域名
-#if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-#  codingUrl=git@e.coding.net:miluluu/miluluu.git
-#else
-#  codingUrl=https://miluluu:${CODING_TOKEN}@e.coding.net/miluluu/miluluu/miluluu.git
-#fi
-#git add -A
-#git commit -m "${msg}"
-#git push -f $codingUrl master # 推送到coding
 
 cd - # 退回开始所在目录
 rm -rf docs/.vuepress/dist
+
